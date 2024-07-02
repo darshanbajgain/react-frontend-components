ff8c64ca0ef4ef56dd73a11e31e83731135a87f0
Darshan Bajgain <darshanbajgain90@gmail.com>
2024-07-02 10:40:58 +0545

    pc info added

--- react-image-carousel/src/components/Product.jsx
+++ react-image-carousel/src/components/Product.jsx
@@ -7,17 +7,17 @@ const Product = () => (
     <div className="flex gap-4 mt-6">
       <div className="w-2/5">
         <img
-          alt="Product"
-          src="https://ik.imagekit.io/d9mvewbju/SmileCart/thumbnail_61_7PaLfb.jpg"
-          className="w-10/12 h-64"
+          alt="PC"
+          src="https://s.alicdn.com/@sc04/kf/H8b92917429e046099a56d5aa27c360b9I.jpg"
+          className="w-96 h-64"
         />
       </div>
       <div className="w-3/5 space-y-4">
         <p>
-          Infinix Inbook X1 Ci3 10th 8GB 256GB 14 Win10 Grey - 1 Year Warranty.
+          Dummy PC  - 1 Year Warranty.
         </p>
-        <p>MRP: $395.97</p>
-        <p className="font-semibold">Offer price: $374.43</p>
+        <p>MRP: RS.45,000</p>
+        <p className="font-semibold">Offer price: Rs.41,000</p>
         <p className="font-semibold text-green-600">6% off</p>
       </div>
     </div>
